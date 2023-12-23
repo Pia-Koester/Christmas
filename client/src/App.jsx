11acfd9cb563d70f1984c4f6e8a2e0fca177d302
Pia <pia.koester@hey.com>
2023-12-23 14:32:56 +0100

    add background-image, flexbox logic and possibility to change flexbox values using dropdown

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [flexDirection, setFlexDirection] = useState("row");
+  const [justifyContent, setJustifyContent] = useState("flex-start");
+  const [alignItems, setAlignItems] = useState("flex-start");
+
+  const handleFlexDirectionChange = (e) => {
+    setFlexDirection(e.target.value);
+  };
+
+  const handleJustifyContentChange = (e) => {
+    setJustifyContent(e.target.value);
+  };
+
+  const handleAlignItemsChange = (e) => {
+    setAlignItems(e.target.value);
+  };
+
+  const flexContainerStyle = {
+    display: "flex",
+    flexDirection: flexDirection,
+    justifyContent: justifyContent,
+    alignItems: alignItems,
+    width: "650px",
+    height: "650px",
+    backgroundImage: `url("./public/21159783_6350849.webp")`,
+    backgroundRepeat: "no - repeat",
+    backgroundPosition: "center",
+    backgroundSize: "contain",
+  };
+
+  const flexItemStyle = {
+    width: "80px",
+    height: "80px",
+    margin: "20px",
+  };
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1>Frohe Weihnachten! </h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Wähle die passenden Flex-Box Werte um die Geschenke unter dem Baum zu
+          platzieren.{" "}
         </p>
+        <label htmlFor="flexDirection">Flex Direction:</label>
+        <select
+          id="flexDirection"
+          value={flexDirection}
+          onChange={handleFlexDirectionChange}
+        >
+          <option value="row">Row</option>
+          <option value="column">Column</option>
+        </select>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div>
+        <label htmlFor="justifyContent">Justify Content:</label>
+        <select
+          id="justifyContent"
+          value={justifyContent}
+          onChange={handleJustifyContentChange}
+        >
+          <option value="flex-start">Flex Start</option>
+          <option value="center">Center</option>
+          <option value="flex-end">Flex End</option>
+          <option value="space-between">Space Between</option>
+          <option value="space-around">Space Around</option>
+          <option value="space-evenly">Space-Evenly</option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="alignItems">Align Items:</label>
+        <select
+          id="alignItems"
+          value={alignItems}
+          onChange={handleAlignItemsChange}
+        >
+          <option value="flex-start">Flex Start</option>
+          <option value="center">Center</option>
+          <option value="flex-end">Flex End</option>
+        </select>
+      </div>
+
+      <div style={flexContainerStyle}>
+        <img style={flexItemStyle} src="./public/presents.png" />
+        <img style={flexItemStyle} src="./public/presents.png" />
+        <img style={flexItemStyle} src="./public/presents.png" />
+      </div>
+    </div>
+  );
+};
+
+export default App;
